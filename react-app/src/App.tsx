54e08a20dcc267ecae66d76c9111b99e6ebe676d
Sergey Kolokolov <serg.a.bells@gmail.com>
2024-12-29 16:38:29 -0800

    adds react-app

---
+++ react-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { TestButton } from './TestButton';
+import { appStyles } from './styles';
+
+export const App: React.FC = () => {
+    const [clicked, setClicked] = useState(false);
+
+    return (
+        <div style={appStyles.container}>
+            <TestButton 
+                onClick={() => setClicked(!clicked)}
+                text={clicked ? 'OK' : 'Test'}
+            />
+        </div>
+    );
+};
